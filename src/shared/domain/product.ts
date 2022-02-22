03fe66ac8c48dfde06cb37a8847a9e11b68bfe2a
Daniel Figueiredo <daniel.figueiredo@compasso.com.br>
2022-02-22 12:55:21 -0300

    chore: create product domain

---
+++ src/shared/domain/product.ts
@@ -0,0 +1,7 @@
+export class Product {
+  id!: string
+  productId!: string
+  establishmentId!: string
+  title!: string
+  description!: string
+}
---
+++ src/shared/infra/database/mongodb/schemas/product-schema.ts
@@ -0,0 +1,15 @@
+﻿import * as mongoose from 'mongoose'
+import { Product } from '../../../../domain/product'
+
+const ProductSchema = new mongoose.Schema(
+  {
+    id: { type: String },
+    productId: { type: String },
+    establishmentId:  { type: String },
+    title:  { type: String },
+    description:  { type: String },
+  },
+  { timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' }, _id: false }
+)
+
+export const ProductModel = mongoose.model<Product>('Product', ProductSchema)
