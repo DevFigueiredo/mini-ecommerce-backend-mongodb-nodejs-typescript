bfe14be831a725a850e8197ef39e24ae434038f2
Daniel Figueiredo <daniel.figueiredo@compasso.com.br>
2022-02-23 18:14:39 -0300

    refact: refact consumer add product

--- src/modules/queue/consumers/add-product-order-consumer.ts
+++ src/modules/queue/consumers/add-product-order-consumer.ts
@@ -1,27 +1,24 @@
 // Load the AWS SDK for Node.js
-import { container } from '../../shared/container'
-import { IAddProductOrderUseCase } from '../../shared/protocols/useCases/order/add-product-order-use-cases-interface'
-import { IQueue } from '../protocols/queue-interface'
+import { container } from '../../../shared/container/'
+import { IAddProductOrderUseCase } from '../../../shared/protocols/useCases/order/add-product-order-use-cases-interface'
+import { IDeleteQueueUseCase } from '../../../shared/protocols/useCases/queue/delete-queue-use-case'
+import { IReceiveQueueUseCase } from '../../../shared/protocols/useCases/queue/receive-queue-use-case'
 
 const queueURL = 'https://sqs.sa-east-1.amazonaws.com/011292373750/add-product-order'
 
 async function receive (): Promise<void> {
   const addProductOrderUseCase: IAddProductOrderUseCase = container.resolve('addProductOrderUseCase')
-  const queue: IQueue = container.resolve('queue')
-  const messages = await queue.receive(queueURL)
+  const receiveQueueUseCase: IReceiveQueueUseCase = container.resolve('receiveQueueUseCase')
+  const deleteQueueUseCase: IDeleteQueueUseCase = container.resolve('deleteQueueUseCase')
+  const messages = await receiveQueueUseCase.execute({ queueURL })
   if (!messages) return
   for (const message of messages) {
     const { quantity, productId, customerId } = JSON.parse(message.Body)
     await addProductOrderUseCase.execute({ entity: { quantity, productId }, params: { customerId } })
-    await queue.delete(queueURL, message.ReceiptHandle)
+    await deleteQueueUseCase.execute({ queueURL, messageId: message.ReceiptHandle })
 
     console.log(`Product ${productId as string} inserted on Order Bag`)
   }
 }
 
 void receive()
-
-// await sqs.deleteMessage({
-//   QueueUrl: queueURL,
-//   ReceiptHandle: data.Messages[0].ReceiptHandle
-// }).promise()
